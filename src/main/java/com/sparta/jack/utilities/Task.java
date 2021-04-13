99ee578466d5075a7debefbb31eb902669e78ca3
jackingham <jackingham1998@gmail.com>
2021-04-13 15:59:09 +0100

    Added exception handling

--- src/main/java/com/sparta/jack/utilities/Task.java
+++ src/main/java/com/sparta/jack/utilities/Task.java
@@ -5,9 +5,11 @@ import com.sparta.jack.database.EmployeeDTO;
 import com.sparta.jack.database.EmployeesHashMap;
 import java.sql.PreparedStatement;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 
 public class Task implements Runnable {
+
 
 
     @Override
@@ -15,17 +17,27 @@ public class Task implements Runnable {
         EmployeeDAO myEmployeeDAO = new EmployeeDAO();
         PreparedStatement ps = myEmployeeDAO.makePreparedStatement();
         Timer.startTimer();
+        int recordsInserted=0;
         while (EmployeesHashMap.it.hasNext()) {
-            EmployeeDTO newEmployee = getNextEmployee();
-            myEmployeeDAO.InsertEmployee(newEmployee, ps);
+            try {
+                EmployeeDTO newEmployee = getNextEmployee();
+                myEmployeeDAO.InsertEmployee(newEmployee, ps);
+                recordsInserted++;
+            } catch (NullPointerException e) {
+                System.out.println("null pointer caught");
+            }
         }
 
-        Printer.printMessage(Thread.currentThread().getName()+" finished in "+Timer.stopTimerAndGetTime()+" seconds");
+        Printer.printMessage(Thread.currentThread().getName()+" finished in "+Timer.stopTimerAndGetTime()+" seconds and inserted "+recordsInserted);
     }
 
-    public synchronized static EmployeeDTO getNextEmployee(){
-        Map.Entry pair = (Map.Entry)EmployeesHashMap.it.next();
-        return (EmployeeDTO) pair.getValue();
+    public synchronized static EmployeeDTO getNextEmployee() throws NullPointerException{
+        if (EmployeesHashMap.it.hasNext()){
+            Map.Entry pair = (Map.Entry) EmployeesHashMap.it.next();
+            return (EmployeeDTO) pair.getValue();
+        } else {
+            throw new NullPointerException();
+        }
     }
 
 
